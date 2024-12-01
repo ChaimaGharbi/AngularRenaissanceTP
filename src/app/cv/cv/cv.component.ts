4760d4b7c2349217fa38e75b74fb6f72a22054e0
Mohamed Fedi Adel <mohamedfedi.adel@medius.com>
2024-12-01 17:27:20 +0100

    routing

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -3,6 +3,8 @@ import { Cv } from "../model/cv";
 import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
@@ -10,18 +12,39 @@ import { CvService } from "../services/cv.service";
 })
 export class CvComponent {
   cvs: Cv[] = [];
+  juniorCvs: Cv[] = [];
+  seniorCvs: Cv[] = [];
   selectedCv: Cv | null = null;
   /*   selectedCv: Cv | null = null; */
   date = new Date();
+  type: string = "juniors";
 
   constructor(
     private logger: LoggerService,
     private toastr: ToastrService,
-    private cvService: CvService
+    private cvService: CvService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {
+    this.type = this.activatedRoute.snapshot.paramMap.get("type") || this.type; 
+
+    this.activatedRoute.params.subscribe(params => {
+      this.type = params['type'] || this.type;
+      console.log('Updated type:', this.type);
+    });
+
     this.cvService.getCvs().subscribe({
       next: (cvs) => {
         this.cvs = cvs;
+        this.juniorCvs = [];
+        this.seniorCvs = [];
+        cvs.forEach(cv => {
+          if (cv.age < 40) {
+            this.juniorCvs.push(cv);
+          } else {
+            this.seniorCvs.push(cv);
+          }
+        });
       },
       error: () => {
         this.cvs = this.cvService.getFakeCvs();
@@ -33,5 +56,13 @@ export class CvComponent {
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
     this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+  }
+
+  getToJuniors(){
+    this.router.navigate(["cv", { type: "juniors" }]);
+  }
+
+  getToSeniors(){
+    this.router.navigate(["cv" , { type: "seniors" }]);
   }
 }
