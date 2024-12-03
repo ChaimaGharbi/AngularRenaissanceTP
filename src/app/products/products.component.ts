32d2207b6082f5568cb5c59bf876acbe78b236b1
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-12-04 00:25:41 +0100

    refactor: use tap for the side effect

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import {
   BehaviorSubject,
   concatMap,
   Observable,
   scan,
   takeWhile,
+  tap,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
@@ -17,31 +18,35 @@ import { ProductApiResponse } from "./dto/product-api-response.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent implements OnInit {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
   private productService = inject(ProductService);
   products$!: Observable<Product[]>;
   settings$ = new BehaviorSubject<Settings>({ limit: 12, skip: 0 });
 
   constructor() { }
+
   ngOnInit() {
-    let totalProducts = Infinity; 
-  
+    let totalProducts: number;
+
     this.products$ = this.settings$.pipe(
-      concatMap((settings) => this.productService.getProducts(settings)), 
-      scan((acc: Product[], value: ProductApiResponse) => {
-        const updatedProducts = [...acc, ...value.products];
-        totalProducts = value.total;  
-        return updatedProducts.length > totalProducts ? updatedProducts.slice(0, totalProducts) : updatedProducts;
-      }, []),
-      takeWhile((products) => products.length < totalProducts, true) 
+      concatMap((settings) => this.productService.getProducts(settings)),
+      tap((value: ProductApiResponse) => {
+        totalProducts = value.total;
+      }),
+      scan(
+        (
+          acc: Product[],
+          value: ProductApiResponse,
+        ) => [...acc, ...value.products],
+        [],
+      ),
+      takeWhile((products) => products.length < totalProducts, true),
     );
   }
 
   loadMoreProducts() {
     this.settings$.next({
       ...this.settings$.value,
-      skip: this.settings$.value.skip + this.settings$.value.limit, 
+      skip: this.settings$.value.skip + this.settings$.value.limit,
     });
   }
 }
