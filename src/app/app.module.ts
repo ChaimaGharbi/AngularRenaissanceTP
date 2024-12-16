ffe09937a318ffdf8e7b71f66f3e900d1a2d26d4
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-12-16 05:14:22 +0100

    perf: use pipe and memoization for fibonacci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,12 +40,8 @@ import { UserListComponent } from "./optimizationPattern/user-list/user-list.com
 import { ProductsComponent } from "./products/products.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { SliderComponent } from "./rxjs/slider/slider.component";
-import { AutocompleteListComponent } from "./cv/autocomplete-list/autocomplete-list.component";
-import { AutocompleteItemComponent } from "./cv/autocomplete-item/autocomplete-item.component";
-import { MasterDetailsCvComponent } from "./mycv/master-details-cv/master-details-cv.component";
-import { List2Component } from "./mycv/list/list.component";
-import { DetailsCv2Component } from "./mycv/details-cv/details-cv.component";
 import { UserItemComponent } from "./optimizationPattern/user-item/user-item.component";
+import { FiboPipe } from "./pipes/fibo.pipe";
 
 @NgModule({
   declarations: [
@@ -76,6 +72,7 @@ import { UserItemComponent } from "./optimizationPattern/user-item/user-item.com
     UserListComponent,
     ProductsComponent,
     UserItemComponent,
+    FiboPipe,
   ],
   bootstrap: [AppComponent],
   imports: [
--- src/app/optimizationPattern/user-item/user-item.component.ts
+++ src/app/optimizationPattern/user-item/user-item.component.ts
@@ -1,26 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { User } from '../users.service';
-
-export const fibonnaci = (n: number): number => {
-  if (n==1 || n==0) {
-    return 1;
-  }
-  return fibonnaci(n-1) + fibonnaci(n-2);
-}
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { User } from "../users.service";
 
 @Component({
-  selector: 'app-user-item',
-  templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-user-item",
+  templateUrl: "./user-item.component.html",
+  styleUrls: ["./user-item.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserItemComponent {
-  @Input() user: User | null = null;
-
-  fibo(n: number): number {
-    const fib = fibonnaci(n);
-    console.log({n, fib});
-
-    return fib;
-  }
+  @Input({ required: true })
+  user!: User;
 }
